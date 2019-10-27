f2d9f8ed17205a85e1501ceab1bacf38007736bd
Your Name <you@example.com>
2019-10-27 15:34:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from "constants";
+
 // Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+        for let i = 0; i < namesArray.length; i++) {
+            thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` );
+        }
+        return thankYouCards;
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+}  
